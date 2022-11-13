c23f7a1edfb9c2423acc737ff49fb1b8f36b92f1
Zaquiel Rodriguez Arce <zaqroar@gmail.com>
2022-11-13 16:55:57 +0100

    feat: finish cleaning files

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,91 +1,12 @@
 import { View, Text, TextInput, SafeAreaView, ScrollView, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline';
-import Categories from '../components/Categories';
-import FeaturedRow from '../components/FeaturedRow';
-import sanityClient from '../sanity';
 import React, { useLayoutEffect, useEffect, useState } from 'react';
 
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
-  const [featuredCategories, setFeaturedCategories] = useState([]);
-
-  useEffect(() => {
-    sanityClient.fetch(`
-     *[_type == "featured"] {
-        ...,
-        restaurants[]->{
-          ...,
-          dishes[]->
-        }
-      }`).then((data) => {
-      setFeaturedCategories(data);
-    });
-  }, []);
-
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }, []);
-
   return (
-    <SafeAreaView className="bg-white pt-5">
-      {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-        <Image
-          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
-          source={{
-            uri: 'https://links.papareact.com/wru'
-          }}
-        />
-        <View className="flex-1">
-          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-          <Text className="font-bold text-xl">
-            Current Location
-            <ChevronDownIcon size={20} color="#00CCBB" />
-          </Text>
-        </View>
-        <UserIcon size={35} color="#00CCBB" />
-      </View>
-
-      {/* Search */}
-      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-          <SearchIcon color="gray" size={20} />
-          <TextInput
-            keyboardType="default"
-            placeholder="Restaurants and cuisines"
-          />
-        </View>
-        <AdjustmentsIcon color="#00CCBB" />
-      </View>
-
-      {/* Body */}
-      <ScrollView
-        className="bg-gray-100"
-        contentContainerStyle={{
-          paddingBottom: 100
-        }}
-      >
-        {/* Categories */}
-        <Categories />
-
-        {
-          featuredCategories.map(({ name, short_description, _id, _type }) => (
-            <FeaturedRow
-              description={short_description}
-              id={_id}
-              featuredCategory={_type}
-              title={name}
-            />
-          ))
-        }
-
-      </ScrollView>
-    </SafeAreaView>
+    <View>
+      <Text>HOME SCREEN</Text>
+    </View>
   );
 }
 
